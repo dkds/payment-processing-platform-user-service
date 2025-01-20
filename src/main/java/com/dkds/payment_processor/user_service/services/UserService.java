c4d844544ade87dd0bc4edbad23769777f56fa5e
dkds <dkds@users.noreply.github.com>
2025-01-20 17:02:18 +0530

    Refactor code to resolve running errors

--- src/main/java/com/dkds/payment_processor/user_service/services/UserService.java
+++ src/main/java/com/dkds/payment_processor/user_service/services/UserService.java
@@ -2,16 +2,32 @@ package com.dkds.payment_processor.user_service.services;
 
 import com.dkds.payment_processor.user_service.entities.User;
 import com.dkds.payment_processor.user_service.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
+@Scope("prototype")
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        log.info("Bean is initialized");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("Bean is about to be destroyed");
+    }
 
     public User registerUser(User user) {
         return userRepository.save(user);
