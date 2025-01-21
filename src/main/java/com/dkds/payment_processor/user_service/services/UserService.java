371e943fc3db673129d0d221d97ebe7d2c361a85
dkds <dkds@users.noreply.github.com>
2025-01-21 12:38:21 +0530

    Refactor to extract service interfaces

--- src/main/java/com/dkds/payment_processor/user_service/services/UserService.java
+++ src/main/java/com/dkds/payment_processor/user_service/services/UserService.java
@@ -1,50 +1,15 @@
 package com.dkds.payment_processor.user_service.services;
 
 import com.dkds.payment_processor.user_service.entities.User;
-import com.dkds.payment_processor.user_service.repositories.UserRepository;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Slf4j
-@Scope("prototype")
-@Service
-public class UserService {
+public interface UserService {
+    User registerUser(User user);
 
-    @Autowired
-    private UserRepository userRepository;
+    Optional<User> findUserByUsername(String username);
 
-    @PostConstruct
-    public void init() {
-        log.info("Bean is initialized");
-    }
+    Optional<User> findUserByEmail(String email);
 
-    @PreDestroy
-    public void destroy() {
-        log.info("Bean is about to be destroyed");
-    }
-
-    public User registerUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public Optional<User> findUserByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    public Optional<User> findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-
-    public void updateWalletBalance(Long userId, Double amount) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-        user.setWalletBalance(user.getWalletBalance() + amount);
-        userRepository.save(user);
-    }
+    void updateWalletBalance(Long userId, Double amount);
 }
---
+++ src/main/java/com/dkds/payment_processor/user_service/services/impl/SpringDataUserService.java
@@ -0,0 +1,55 @@
+package com.dkds.payment_processor.user_service.services.impl;
+
+import com.dkds.payment_processor.user_service.entities.User;
+import com.dkds.payment_processor.user_service.repositories.UserRepository;
+import com.dkds.payment_processor.user_service.services.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Scope("prototype") // adding here to check the behavior of different scopes
+@Service
+public class SpringDataUserService implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        log.info("Bean is initialized");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("Bean is about to be destroyed");
+    }
+
+    @Override
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void updateWalletBalance(Long userId, Double amount) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.setWalletBalance(user.getWalletBalance() + amount);
+        userRepository.save(user);
+    }
+}
