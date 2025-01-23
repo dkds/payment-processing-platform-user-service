b671e2665cc39087b6d43c5ac7c7da22fa758eae
dkds <dkds@users.noreply.github.com>
2025-01-23 14:05:39 +0530

    Add initial controller tests (broken)

---
+++ src/test/java/com/dkds/payment_processor/user_service/controllers/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.dkds.payment_processor.user_service.controllers;
+
+import com.dkds.payment_processor.user_service.config.JwtUtil;
+import com.dkds.payment_processor.user_service.entities.User;
+import com.dkds.payment_processor.user_service.services.UserService;
+import com.dkds.payment_processor.user_service.services.impl.SpringDataUserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@Import(JwtUtil.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void shouldReturnUserByUsername() throws Exception {
+        String testUsername = "test@test.com";
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setUsername(testUsername);
+        when(userService.findUserByUsername(testUsername)).thenReturn(Optional.of(mockUser));
+
+        mockMvc.perform(get("/api/users/" + testUsername))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(testUsername));
+    }
+}
